4842b6972597552d60d921b27ff30e6a42cde030
Taku Nakagawa <taku_nakagawa@change-jp.com>
2018-10-08 13:52:44 +0900

    カメラの実装

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,19 +4,34 @@ import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  myPhoto : any;
+
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  constructor(private camera : Camera) {
 
   }
-  navigateCamera() {
-    console.log("TEST!");
+  navigateCamera() { 
+    this.camera.getPicture(this.options).then((imageData) => {
+      this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log("カメラが立ち上がりません！");
+    });
   }
 }
